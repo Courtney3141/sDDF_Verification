ca5ba82512cfab2bfd8fcb54fd622331c5a4f820
ralismark <13449732+ralismark@users.noreply.github.com>
2024-03-04 15:59:29 +1100

    Make tcp echo server support multiple clients

--- echo_server/tcp_echo_socket.c
+++ echo_server/tcp_echo_socket.c
@@ -8,61 +8,39 @@
 #include "lwip/pbuf.h"
 #include "lwip/tcp.h"
 
-uint64_t tx_bytes_total;
+// At most ECHO_QUEUE_SIZE - 1 bytes can be in the queue
+#define ECHO_QUEUE_SIZE (TCP_WND + 1)
 
-// Send ring buffer for holding sent data until it is acknowledged.
-//
-// * Tail should never overtake head, and tail == head for empty queue.
-// * The TCP window should equal the remaining space in this ring buffer.
-//
-// TODO make this per-connection instead of global.
-struct {
-    // At most ECHO_QUEUE_SIZE - 1 bytes can be in the queue
-    #define ECHO_QUEUE_SIZE (TCP_WND + 1)
-
-    char buf[ECHO_QUEUE_SIZE];
+struct echo_state {
+    // sending ring buffer
     size_t tail; // data gets added at tail
-    size_t head; // acknowledged data gets removed from head
-} static echo_queue;
+    size_t head; // moved forward for acknowledged data
+    char buf[ECHO_QUEUE_SIZE];
+};
 
-// Total free space remaining
-static size_t echo_queue_space()
+LWIP_MEMPOOL_DECLARE(
+    tcp_echo_state,
+    8,
+    sizeof(struct echo_state),
+    "TCP echo server connection state"
+);
+
+static size_t queue_space(struct echo_state* state)
 {
-    return (echo_queue.head + ECHO_QUEUE_SIZE - echo_queue.tail - 1) % ECHO_QUEUE_SIZE;
+    return (state->head + ECHO_QUEUE_SIZE - state->tail - 1) % ECHO_QUEUE_SIZE;
 }
 
-// Number of bytes that can be added contiguously to tail
-static size_t echo_queue_contiguous_space()
+static size_t queue_cont_space(struct echo_state* state)
 {
-    if (echo_queue.tail >= echo_queue.head) return ECHO_QUEUE_SIZE - echo_queue.tail;
-    return echo_queue.head - echo_queue.tail - 1;
-}
-
-// print statistics
-static err_t tcp_echo_report(void* arg, struct tcp_pcb* pcb)
-{
-    printf("%u | tx=%d queue={tail=%d head=%d space=%d} rcv_wnd=%d sndbuf=%d queuelen=%d\n",
-        sys_now(),
-        tx_bytes_total,
-
-        echo_queue.tail,
-        echo_queue.head,
-        echo_queue_space(),
-
-        pcb->rcv_wnd,
-        tcp_sndbuf(pcb),
-        pcb->snd_queuelen
-    );
-    return ERR_OK;
+    if (state->tail >= state->head) return ECHO_QUEUE_SIZE - state->tail;
+    return state->head - state->tail - 1;
 }
 
 static err_t tcp_echo_sent(void* arg, struct tcp_pcb* pcb, u16_t len)
 {
-    tx_bytes_total += len;
+    struct echo_state* state = arg;
 
-    /* printf("----- %u: sent: len=%d\n", sys_now(), len); */
-
-    echo_queue.head = (echo_queue.head + len) % ECHO_QUEUE_SIZE;
+    state->head = (state->head + len) % ECHO_QUEUE_SIZE;
 
     // tcp_recved is only for increasing the TCP window, and isn't required to
     // ACK incoming packets (that is done automatically on receive).
@@ -71,41 +49,37 @@ static err_t tcp_echo_sent(void* arg, struct tcp_pcb* pcb, u16_t len)
     return ERR_OK;
 }
 
-static err_t tcp_echo_recv(void *arg, struct tcp_pcb *pcb, struct pbuf *p, err_t err)
+static err_t tcp_echo_recv(void* arg, struct tcp_pcb* pcb, struct pbuf* p, err_t err)
 {
+    struct echo_state* state = arg;
+
     if (p == NULL) {
-        printf("TCP closing\n");
+        // closing
+        printf("tcp_echo[%p]: closing\n", state);
+
+        // TODO is this a use-after-free?
+        LWIP_MEMPOOL_FREE(tcp_echo_state, state);
+
         err = tcp_close(pcb);
         if (err) {
-            printf("TCP close error: %s\n", lwip_strerr(err));
+            printf("tcp_echo[%p]: close error: %s\n", state, lwip_strerr(err));
             return err;
         }
         return ERR_OK;
     }
     if (err) {
-        printf("TCP recv error: %s\n", lwip_strerr(err));
+        printf("tcp_echo[%p]: recv error: %s\n", state, lwip_strerr(err));
         return err;
     }
 
-    /*
-    printf("----- %u: recv: tot_len=%d queuelen=%d sndbuf=%d qhead=%d qtail=%d qcap=%d\n",
-        sys_now(),
-        p->tot_len,
-        pcb->snd_queuelen,
-        tcp_sndbuf(pcb),
-        echo_queue.head,
-        echo_queue.tail,
-        echo_queue_space()
-    );
-    */
-
     assert(p->tot_len > 0);
 
-    const u16_t capacity = min(min(echo_queue_space(), tcp_sndbuf(pcb)), p->tot_len);
+    const size_t capacity = min(min(queue_space(state), tcp_sndbuf(pcb)), p->tot_len);
     if (p->tot_len > capacity) {
-        printf("%u: Can't handle packet of %d bytes: echo_queue_space=%d sndbuf=%d snd_queuelen=%d\n",
-            sys_now(),
-            echo_queue_space(),
+        printf("tcp_echo[%p]: can't handle packet of %d bytes: queue_space=%lu sndbuf=%d snd_queuelen=%d\n",
+            state,
+            p->tot_len,
+            queue_space(state),
             tcp_sndbuf(pcb),
             pcb->snd_queuelen
         );
@@ -117,24 +91,23 @@ static err_t tcp_echo_recv(void *arg, struct tcp_pcb *pcb, struct pbuf *p, err_t
         return ERR_MEM;
     }
 
-    u16_t offset = 0;
+    size_t offset = 0;
     while (offset < capacity) {
         const u16_t copied_len = pbuf_copy_partial(
             p,
-            echo_queue.buf + echo_queue.tail,
-            min(echo_queue_contiguous_space(), capacity - offset),
+            state->buf + state->tail,
+            min(queue_cont_space(state), capacity - offset),
             offset
         );
 
-        err = tcp_write(pcb, echo_queue.buf + echo_queue.tail, copied_len, 0);
+        err = tcp_write(pcb, state->buf + state->tail, copied_len, 0);
         if (err) {
-            printf("Failed to tcp_write: %s\n", lwip_strerr(err));
-            printf("  snd_queuelen=%d snd_buf=%d\n", pcb->snd_queuelen, pcb->snd_buf);
+            printf("tcp_echo[%s]: failed to write: %s\n", state, lwip_strerr(err));
             assert(false);
         }
 
         offset += copied_len;
-        echo_queue.tail = (echo_queue.tail + copied_len) % ECHO_QUEUE_SIZE;
+        state->tail = (state->tail + copied_len) % ECHO_QUEUE_SIZE;
     }
 
     tcp_output(pcb);
@@ -143,47 +116,66 @@ static err_t tcp_echo_recv(void *arg, struct tcp_pcb *pcb, struct pbuf *p, err_t
     return ERR_OK;
 }
 
-static void tcp_echo_err(void *arg, err_t err)
+static void tcp_echo_err(void* arg, err_t err)
 {
-    printf("%u | TCP error: %s\n", sys_now(), lwip_strerr(err));
+    struct echo_state* state = arg;
+
+    printf("tcp_echo[%p]: %s\n", arg, lwip_strerr(err));
+
+    LWIP_MEMPOOL_FREE(tcp_echo_state, state);
 }
 
-static err_t tcp_accept_callback(void *arg, struct tcp_pcb *pcb, err_t err) {
-    printf("%u | TCP connected\n", sys_now());
+static err_t tcp_echo_accept(void* arg, struct tcp_pcb* pcb, err_t err)
+{
+    struct echo_state* state = LWIP_MEMPOOL_ALLOC(tcp_echo_state);
+    if (state == NULL) {
+        printf("tcp_echo: failed to alloc state\n");
+        return ERR_MEM;
+    }
+
+    printf("tcp_echo[%p]: accept from %s port %d\n",
+        state,
+        ipaddr_ntoa(&pcb->remote_ip),
+        pcb->remote_port
+    );
+
+    state->tail = 0;
+    state->head = 0;
 
     tcp_nagle_disable(pcb);
-
-    tcp_arg(pcb, (void *)pcb);
-    tcp_recv(pcb, tcp_echo_recv);
+    tcp_arg(pcb, state);
     tcp_sent(pcb, tcp_echo_sent);
+    tcp_recv(pcb, tcp_echo_recv);
     tcp_err(pcb, tcp_echo_err);
-    tcp_poll(pcb, tcp_echo_report, 2);
 
     return ERR_OK;
 }
 
 int setup_tcp_socket(void)
 {
-    struct tcp_pcb *tcp_socket;
+    LWIP_MEMPOOL_INIT(tcp_echo_state);
 
-    tcp_socket = tcp_new_ip_type(IPADDR_TYPE_V4);
-    if (tcp_socket == NULL) {
-        printf("Failed to open a TCP socket");
+    struct tcp_pcb* pcb;
+
+    pcb = tcp_new_ip_type(IPADDR_TYPE_V4);
+    if (pcb == NULL) {
+        printf("Failed to open TCP echo socket\n");
         return -1;
     }
 
-    err_t error = tcp_bind(tcp_socket, IP_ANY_TYPE, TCP_ECHO_PORT);
+    err_t error = tcp_bind(pcb, IP_ANY_TYPE, TCP_ECHO_PORT);
     if (error) {
-        printf("Failed to bind TCP echo socket: %d\n", (int) error);
+        printf("Failed to bind TCP echo socket: %s\n", lwip_strerr(error));
         return -1;
     }
 
-    tcp_socket = tcp_listen_with_backlog_and_err(tcp_socket, 1, &error);
+    pcb = tcp_listen_with_backlog_and_err(pcb, 1, &error);
     if (error) {
-        printf("Failed to listen on TCP echo socket: %d\n", (int) error);
+        printf("Failed to listen on TCP echo socket: %s\n", lwip_strerr(error));
         return -1;
     }
-    tcp_accept(tcp_socket, tcp_accept_callback);
+
+    tcp_accept(pcb, tcp_echo_accept);
 
     return 0;
 }
